b2384d43e38366e54bfead901ad2294f56c14876
AbdulRafayPython <scig27267@gmail.com>
2025-05-05 00:56:19 +0500

    Initial

--- app.py
+++ app.py
@@ -212,4 +212,6 @@ def format_timestamp(timestamp):
     return timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
+    import eventlet
+    eventlet.monkey_patch()
     socketio.run(app, debug=True, host='127.0.0.1', port=8000)
